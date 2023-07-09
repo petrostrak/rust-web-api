4fae9e8c6673b12fa92b58a06f2113c08ab3d469
petrostrak <pit.trak@gmail.com>
2023-07-09 16:33:24 +0300

    Add clap command tree for users

--- src/bin/cli.rs
+++ src/bin/cli.rs
@@ -1,3 +1,44 @@
+use clap::{Arg, Command};
+
 extern crate rustwebapi;
 
-fn main() {}
+fn main() {
+    let matches = Command::new("rustwebapi")
+        .version("1.0")
+        .propagate_version(true)
+        .subcommand_required(true)
+        .arg_required_else_help(true)
+        .subcommand(
+            Command::new("users")
+                .about("User administration commands")
+                .arg_required_else_help(true)
+                .subcommand(
+                    Command::new("create")
+                        .about("Create a new user")
+                        .arg(Arg::new("username").required(true))
+                        .arg(Arg::new("password").required(true))
+                        .arg(
+                            Arg::new("roles")
+                                .required(true)
+                                .num_args(1..)
+                                .value_delimiter(','),
+                        ),
+                )
+                .subcommand(Command::new("list").about("List users"))
+                .subcommand(
+                    Command::new("delete")
+                        .about("Deletes an existing user")
+                        .arg(Arg::new("id").required(true)),
+                ),
+        )
+        .get_matches();
+    match matches.subcommand() {
+        Some(("users", sub_matches)) => match sub_matches.subcommand() {
+            Some(("create", sub_matches)) => create_user(),
+            Some(("list", sub_matches)) => list_users(),
+            Some(("delete", sub_matches)) => delete_user(),
+            _ => {}
+        },
+        _ => {}
+    }
+}
